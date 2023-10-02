3811015228c76b21903c3628f29775a57317453d
princehabeeb <habiy3400@gmail.com>
2023-10-02 17:39:15 +0100

    looping and array

---
+++ script.js
@@ -0,0 +1,39 @@
+// let pets = ['eggs', 'fish', 'milk'];
+// let names = ['Rice', 'Beans', 'Meat'];
+// let g= ['tuwo', 'teba', 'sakwara' ];
+// let f= ['miya', 'miya'];
+
+
+// let food = pets.concat(names);
+
+// console.log(food.concat(g).concat(f));
+
+
+
+// pets.pop();
+
+
+// add item to an array
+// push()
+// unshift()
+
+//remove item from an array
+// pop()
+// pets.unshift('girrafe');
+
+
+// console.log(pets);
+
+
+// let age = 10;
+// let newAge = --age;
+
+// console.log(newAge);
+
+
+
+// console.log(i);
+
+for (i = 1; i <= 13; i++){
+  console.log(i + 'I am sorry');
+}
